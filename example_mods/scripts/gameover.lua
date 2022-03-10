7c17c65e0b1174d7e3c3354812f6a19c11fb4fd4
AFatto <94957716+thefatos@users.noreply.github.com>
2022-03-10 16:16:11 +0100

    Add files via upload

---
+++ example_mods/scripts/gameover.lua
@@ -0,0 +1,6 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'tankman-pixel-dead'); --tankman dead thingy
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx'); --mic and tankkman doing the UHHHG thing
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver'); --p i x e l
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd'); --*respawns*
+end	
